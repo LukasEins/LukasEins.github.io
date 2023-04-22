6346d15f14acf8da71eb7b3acaf82084aa4820d2
LukasEins <88452621+LukasEins@users.noreply.github.com>
2023-04-22 16:44:43 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,23 +1,23 @@
 let header = document.querySelector('#intro');
 let anim = [
-    { t: "〘  〙", ms: 250 },
-    { t: "〘 _〙", ms: 250 },
-    { t: "〘  〙", ms: 250 },
-    { t: "〘 _〙", ms: 250 },
-    { t: "〘 L_  〙", ms: 250 },
-    { t: "〘 Lu_  〙", ms: 150 },
-    { t: "〘 Luk_  〙", ms: 150 },
-    { t: "〘 Luka_ 〙", ms: 150 },
-    { t: "〘 Lukas_ 〙", ms: 150 },
-    { t: "〘 LukasE_ 〙", ms: 150 },
-    { t: "〘 LukasEi_ 〙", ms: 150 },
-    { t: "〘 LukasEin_ 〙", ms: 150 },
-    { t: "〘 LukasEins 〙", ms: 150 },,
-    { t: "〘 LukasEins_ 〙", ms: 150 },
-    { t: "〘 LukasEins  〙", ms: 250 },
-    { t: "〘 LukasEins_ 〙", ms: 250 },
-    { t: "{ LukasEins }", ms: 250 },
-    { t: "{ LukasEins }", ms: 250 }
+    { t: "{ }", ms: 250 },
+    { t: "{_}", ms: 250 },
+    { t: "{ }", ms: 250 },
+    { t: "{_}", ms: 250 },
+    { t: "{L_}", ms: 250 },
+    { t: "{Lu_}", ms: 150 },
+    { t: "{Luk_}", ms: 150 },
+    { t: "{Luka_}", ms: 150 },
+    { t: "{Lukas_}", ms: 150 },
+    { t: "{LukasE_}", ms: 150 },
+    { t: "{LukasEi_}", ms: 150 },
+    { t: "{LukasEin_}", ms: 150 },
+    { t: "{LukasEins }", ms: 150 },,
+    { t: "{LukasEins_}", ms: 150 },
+    { t: "{LukasEins }", ms: 250 },
+    { t: "{LukasEins_}", ms: 250 },
+    { t: "{LukasEins}", ms: 250 },
+    { t: "{LukasEins}", ms: 250 }
 ];
 let stepDenominator = 1;
 if (window.localStorage.stepDenominator)
